b6d21c71b447bb5b7853974446cd7393f8ce573f
arena <arenaferreira@gmail.com>
2024-06-12 17:09:13 +0200

    Create scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+
+FONT_LEVEL = ("Courier", 12, "bold")
+FONT_SCORE = ("Courier", 25, "bold")
+FONT_GAME_OVER = ("Courier", 50, "bold")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.level = 1
+        self.create_score()
+
+    def create_score(self):
+        self.penup()
+        self.hideturtle()
+        self.color("grey")
+        self.update_score()
+
+    def update_score(self):
+        self.goto(450, 290)
+        self.write(f"Level: {self.level}", align="right", font=FONT_LEVEL)
+        self.goto(-430, 270)
+        self.write(f"Score: {self.score}", align="left", font=FONT_SCORE)
+
+    def new_score(self):
+        self.score += 4
+        self.clear()
+        self.update_score()
+        return self.score
+
+    def game_over(self):
+        self.goto(0,-50)
+        self.write(f"GAME OVER", align="Center", font=FONT_GAME_OVER)
