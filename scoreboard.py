b0b48ea469fa14d76edf3e5ce1781db68f35a91b
arena <arenaferreira@gmail.com>
2024-06-13 16:43:41 +0200

    Create high level

--- scoreboard.py
+++ scoreboard.py
@@ -4,11 +4,20 @@ FONT_LEVEL = ("Courier", 12, "bold")
 FONT_SCORE = ("Courier", 25, "bold")
 FONT_GAME_OVER = ("Courier", 50, "bold")
 FONT_HIGH = ("Courier", 12, "normal")
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
         self.level = 1
+        try:
+            with open("data.txt") as file:
+                self.high_level = int(file.read())
+        except FileNotFoundError:
+            with open("data.txt", "w") as file:
+                file.write("0")
+                self.high_level = 1
         self.create_score()
 
     def create_score(self):
@@ -22,21 +31,27 @@ class Scoreboard(Turtle):
         self.write(f"Level: {self.level}", align="right", font=FONT_LEVEL)
         self.goto(-430, 270)
         self.write(f"Score: {self.score}", align="left", font=FONT_SCORE)
-        self.goto(430, 250)
-        self.write(f"High Score: 0", align="right", font=FONT_HIGH)
+        self.goto(450, 270)
+        self.write(f"High Level: {self.high_level}", align="right", font=FONT_HIGH)
+        if self.level > self.high_level:
+            self.high_level = self.level
+            self.highest_level()
 
     def new_score(self):
         self.score += 4
         self.clear()
         self.update_score()
-        return self.score
 
     def new_level(self):
         self.level += 1
         self.clear()
         self.update_score()
-        # return self.level
 
+    def highest_level(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_level))
+        self.clear()
+        self.update_score()
 
     def game_over(self):
         self.goto(0,-50)
